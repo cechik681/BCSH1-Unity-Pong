16caa41b087af95654e9afca3857df90e7ec9152
cechik681 <cechik681@gmail.com>
2026-03-19 21:19:27 +0100

    Project and base mechanics

---
+++ Assets/Ball.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float startingSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //QualitySettings.vSyncCount = 0;
+
+        bool isRight = UnityEngine.Random.value >= 0.5;
+
+        float xVelocity = -1f;
+
+        if(isRight == true)
+        {
+            xVelocity = 1f;
+        }
+
+        float yVelocity = UnityEngine.Random.Range(-1, 1);
+
+        rb.linearVelocity = new Vector2(xVelocity * startingSpeed, yVelocity * startingSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/P2.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class P2 : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isPressingUp = Keyboard.current.upArrowKey.isPressed;
+        bool isPressingDown = Keyboard.current.downArrowKey.isPressed;
+
+        if (isPressingUp)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * moveSpeed);
+        }
+        if (isPressingDown)
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * moveSpeed);
+        }
+    }
+}
